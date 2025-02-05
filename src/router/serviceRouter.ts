51c51d2a4c8749f3053d59064742f9aafa12fcd7
kugatheesan <vkugatheesan@gmail.com>
2025-02-05 16:28:46 +0530

    new four service

--- src/router/serviceRouter.ts
+++ src/router/serviceRouter.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { getAllServices, getServicesByCategory, getServiceById } from "../conrollers/serviceController";
+import { getAllServices, getServicesByCategory, getServiceById, createService, createCategory, getAllCategories,  } from "../conrollers/serviceController";
 
 const routers = express.Router();
 
 routers.get("/", getAllServices); // Get all services
 routers.get("/category/:category", getServicesByCategory); // Get services by category
 routers.get("/:id", getServiceById); // Get single service by ID
-
+routers.post("/services", createService)
+routers.post("/addcatgeocary", createCategory)
+routers.get("/all", getAllCategories)
 
 export default routers;
