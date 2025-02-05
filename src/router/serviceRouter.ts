dfa5b5ea91c101af85473b090238d0badb5e5cf5
Kugatheesan <git config --global user.email>
2025-02-05 20:11:38 +0530

    commite new1

--- src/router/serviceRouter.ts
+++ src/router/serviceRouter.ts
@@ -5,7 +5,7 @@ const routers = express.Router();
 
 routers.get("/", getAllServices); // Get all services
 routers.get("/category/:category", getServicesByCategory); // Get services by category
-routers.get("/:id", getServiceById); // Get single service by ID
+routers.get("/:id", getServiceById); // Get single service by I
 
 
 export default routers;
